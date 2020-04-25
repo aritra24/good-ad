238c1d1d56ebbe2da0f3a837cfb98d113e74cb22
aritrab <aritra.basu@citrix.com>
2020-04-26 02:42:33 +0530

    changed freeze

--- create.py
+++ create.py
@@ -1,2 +1,8 @@
 from app import db
+from app.models import *
 db.create_all()
+ngo1 = NGO(name="a_ngo")
+pub1 = Publisher(name="a_publisher")
+db.session.add(ngo1)
+db.session.add(pub1)
+db.session.commit()
